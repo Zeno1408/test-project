feb2f42000cd40fd186dd22dd26724d6e7a74713
ADITYA DILIP <adityadilip1408@gmail.com>
2024-03-31 11:24:36 +0530

    simple api built using fastapi

--- api.py
+++ api.py
@@ -1,16 +1,37 @@
 
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-async def print_message():
-    return {"Message": "Hello World!"}
+async def health_get():
+    return {"Message": "Server is up and running!"}
 
-@app.post("/info")
-async def add_info():
-    person = {
-        "Name": "Aditya",
-        "Age": 24
-    }
-    return person
+books_list = [
+    {"id": 1, "Name": "The Secret History", "Author": "Donna Tart"},
+    {"id": 2, "Name": "Dune", "Author": "Frank Herbert"},
+    {"id": 3, "Name": "Warbreaker", "Author": "Brandon Sanderson"}
+]
+
+@app.post("/books")
+async def add_books_list():
+    return books_list
+
+@app.get("/books")
+async def get_books():
+    return books_list
+
+@app.put("/books/{id}")
+async def update_book_by_id(book_id: int):
+    for book in books_list:
+        if book["id"] == book_id:
+            book["Name"] = "The Goldfinch"
+    return books_list
+
+@app.delete("/books/{id}")
+async def delete_book_by_id(book_id: int):
+    for book in books_list:
+        if book["id"] == book_id:
+            books_list.pop(book_id-1)
+    return books_list
--- test.py
+++
@@ -1,4 +0,0 @@
-
-numbers = [2,3,4,5,6]
-squares = list(map(lambda x: x**2, numbers))
-print(squares)
